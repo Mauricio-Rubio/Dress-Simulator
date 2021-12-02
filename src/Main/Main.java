f265e0db404524df6d0c6d3b37ae49ee4feed44d
Mauricio R.H <mau.r.h54@gmail.com>
2021-12-01 22:06:42 -0600

    Primer commit

---
+++ src/Main/Main.java
@@ -0,0 +1,16 @@
+package Main;
+import Person.Persona;
+/**
+ *
+ * @author Mauricio Rubio Haro
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new Persona();
+    }
+    
+}
